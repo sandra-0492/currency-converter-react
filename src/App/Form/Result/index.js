a3f300949605f8016862fb43563fe810fd5985f6
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-18 23:21:38 +0100

    replaced incorrect parentheses: () with braces {}

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,15 +1,14 @@
 import "./style.css";
 
-export const Result = ({ result }) => {
+export const Result = ({ result }) => (
   <p className="result">
-    {result !== undefined && (
+    {!!result && (
       <>
-        {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
-        {" "}
+        {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
         <strong>
           {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
         </strong>
       </>
     )}
-  </p>;
-};
+  </p>
+);
