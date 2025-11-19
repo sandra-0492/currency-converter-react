bbf6ea8b879897168d53cf4dd96bec1fe53ee43d
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-20 00:44:15 +0100

    removed result function

--- src/App/index.js
+++ src/App/index.js
@@ -5,24 +5,10 @@ import { Clock } from "./Clock";
 import { currencies } from "./currencies";
 
 function App() {
-  const [result, setResult] = useState();
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies
-    .find(({ short }) => short === currency)
-    .rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-  };
-
   return (
     <div className="app">
       <Clock />
-      <Form result={result} calculateResult={calculateResult} />
+      <Form />
     </div>
   );
 }
