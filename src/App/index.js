ffb749d14e505e280604365d242f2f79138cb077
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-20 00:23:03 +0100

    added date and time

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { Form } from "./Form";
+import { Clock } from "./Clock";
 import { currencies } from "./currencies";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
 
   return (
     <div className="app">
+      <Clock />
       <Form result={result} calculateResult={calculateResult} />
     </div>
   );
