cb35a64c65b5966aa8666abc93f2924e7385773e
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-20 00:44:39 +0100

    added result function

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -3,9 +3,22 @@ import { currencies } from "../currencies";
 import { Result } from "./Result";
 import "./style.css";
 
-export const Form = ({ calculateResult, result }) => {
+export const Form = () => {
   const [currency, setCurrency] = useState(currencies[0].short);
   const [amount, setAmount] = useState("");
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+    .find(({ short }) => short === currency)
+    .rate;
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
 
   const onSubmit = (event) => {
     event.preventDefault();
