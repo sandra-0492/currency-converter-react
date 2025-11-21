9f22a24caf3619c8a3661a2d26297cbc4737c682
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-21 22:38:19 +0100

    added useCurrentDate & Wrapper, deleted useState, useEffect, style.css

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import "./style.css";
+import { useCurrentDate } from "./useCurrentDate";
+import { Wrapper } from "./styled"
 
 const formatDate = (date) =>
   date.toLocaleString(undefined, {
@@ -12,23 +12,12 @@ const formatDate = (date) =>
   });
 
 export const Clock = () => {
-  const [date, setDate] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
+  const date = useCurrentDate();
 
   return (
-  <div className="clock">
-    Dzisiaj jest
-    {" "}
+  <Wrapper>
+    Dzisiaj jest{" "}
     {formatDate(date)}
-    </div>
+  </Wrapper>
     )
 };
