43a0fdd841f4192cd86b457841a41b02a04b28e6
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-21 22:35:11 +0100

    added new component - theme

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  color: {
+    black: "#000",
+    boulder: "#777",
+    silver: "#ccc",
+    flax: "#eed187",
+    white: "#fff",
+    ebb: "#e9e4e4",
+    forestGreen: "#386b22",
+  },
+};
