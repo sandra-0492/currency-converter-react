479fbf0171da2e7672e6f806bb323e66ed815f2a
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-24 17:46:00 +0100

    Fixed code formatting

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,24 +2,24 @@ import { createGlobalStyle } from "styled-components";
 import background from "./background.jpg";
 
 export const GlobalStyle = createGlobalStyle`
-html {
-  box-sizing: border-box;
-}
+  html {
+    box-sizing: border-box;
+  }
 
-*,
-::after,
-::before {
+  *,
+  ::after,
+  ::before {
   box-sizing: inherit;
-}
+  }
 
-#root {
-  font-family: "Athiti", sans-serif;
-  background-image: url("${background}");
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  background-size: cover;
-  background-position: center;
-}
+  #root {
+    font-family: "Athiti", sans-serif;
+    background-image: url("${background}");
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background-size: cover;
+    background-position: center;
+  }
 `;
