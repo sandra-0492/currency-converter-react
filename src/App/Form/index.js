4cd6b1e34bd74953a720c7cb7bc930ea8f91930b
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-24 17:51:34 +0100

    added 2 new components to import Loading & Failure, deleted import currencies, const [currency, setAmount], const [amount, setAmount], changed const calculateresult, changed targetAmount in setResult to amount * rate, added ratesData.state loading and failure, added Object.keys, updated info

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,24 +1,32 @@
 import { useState } from "react";
-import { currencies } from "../currencies";
 import { Result } from "./Result";
-import { Button, Field, Header, Info, LabelText } from "./styled";
+import {
+  Button,
+  Field,
+  Header,
+  Info,
+  LabelText,
+  Loading,
+  Failure,
+} from "./styled";
+import { useRatesData } from "./useRatesData";
 
 export const Form = () => {
-  const [currency, setCurrency] = useState(currencies[0].short);
-  const [amount, setAmount] = useState("");
   const [result, setResult] = useState();
+  const ratesData = useRatesData();
 
   const calculateResult = (currency, amount) => {
-    const rate = currencies
-    .find(({ short }) => short === currency)
-    .rate;
+    const rate = ratesData.rates[currency].value;
 
     setResult({
       sourceAmount: +amount,
-      targetAmount: amount / rate,
+      targetAmount: amount * rate,
       currency,
     });
   };
+
+  const [currency, setCurrency] = useState("EUR");
+  const [amount, setAmount] = useState("");
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -28,47 +36,61 @@ export const Form = () => {
   return (
     <form onSubmit={onSubmit}>
       <Header>Przelicznik walut</Header>
-      <p>
-        <label>
-          <LabelText>Kwota w złotówkach*:</LabelText>
-          <Field
-            value={amount}
-            onChange={({ target }) => setAmount(target.value)}
-            placeholder="Wpisz kwotę w złotówkach"
-            type="number"
-            required
-            step="0.01"
-          />
-        </label>
-      </p>
-      <p>
-        <label>
-          <LabelText>Waluta:</LabelText>
-          <Field
-            as="select"
-            value={currency}
-            onChange={({ target }) => setCurrency(target.value)}
-          >
-            {currencies.map((currency) => (
-              <option 
-              key={currency.short} 
-              value={currency.short}>
-              {currency.name}
-              </option>
-            ))}
-          </Field>
-        </label>
-      </p>
-      <p>
-        <Button>Przelicz</Button>
-      </p>
+      {ratesData.state === "loading" ? (
+        <Loading>
+          Chwileczkę... <br />
+          Ładuję kursy walut z currencyapi.com
+        </Loading>
+      ) : ratesData.state === "error" ? (
+        // eslint-disable-next-line jsx-a11y/accessible-emoji
+        <Failure>
+          Hmmm... Coś tu nie gra 😵‍💫 Sprawdż, czy masz połączenie z internetem.
+        </Failure>
+      ) : (
+        <>
+          <p>
+            <label>
+              <LabelText>Kwota w złotówkach*:</LabelText>
+              <Field
+                value={amount}
+                onChange={({ target }) => setAmount(target.value)}
+                placeholder="Wpisz kwotę w złotówkach"
+                type="number"
+                required
+                step="0.01"
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              <LabelText>Waluta:</LabelText>
+              <Field
+                as="select"
+                value={currency}
+                onChange={({ target }) => setCurrency(target.value)}
+              >
+                {!!ratesData.rates &&
+                  Object.keys(ratesData.rates).map((currency) => (
+                    <option 
+                      key={currency} 
+                      value={currency}>
+                      {currency}
+                    </option>
+                  ))}
+              </Field>
+            </label>
+          </p>
+          <p>
+            <Button>Przelicz</Button>
+          </p>
 
-      <Info>
-        Kursy pochodzą ze strony NBP z tabeli nr 192/A/NBP/2025 z dnia
-        03-10-2025
-      </Info>
+          <Info>
+            Kursy walut aktualne na dzień {new Date(ratesData.date).toLocaleString("pl-PL")} z currencyapi.com.
+          </Info>
 
-      <Result result={result} />
+          <Result result={result} />
+        </>
+      )}
     </form>
   );
 };
